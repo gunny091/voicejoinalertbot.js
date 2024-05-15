30914c5a6b0198eb7cf2a6856452dcd94e3c1eec
gunny091 <74476758+gunny091@users.noreply.github.com>
2024-05-16 00:15:18 +0900

    모듈 분리, JSONManager.js 추가, 디데이 반응 추가

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-import { createRequire } from "module";
-const require = createRequire(import.meta.url);
-const config = require("./config.json");
-
 import { Client, Collection, Events, GatewayIntentBits } from "discord.js";
+
+import { JSONManager } from "./modules/JSONManager.js";
+
+const config = new JSONManager("./config.json").get();
+
+import dday from "./features/dday.js";
+import voicealert from "./features/voicealert.js";
 
 const client = new Client({
   intents: [
@@ -13,75 +16,11 @@ const client = new Client({
   ],
 });
 
-async function findChannel(Id) {
-  try {
-    const channel = await client.channels.fetch(Id);
-    return channel;
-  } catch (e) {
-    return null;
-  }
-}
-
-function replaceAll(str, filter) {
-  let newStr = str;
-  for (const key of filter.keys()) {
-    newStr = newStr.split(key).join(filter.get(key));
-  }
-  return newStr;
-}
-
-async function checkDDay() {
-  const date = new Date();
-  if (
-    date.getHours() == config.dday.hour &&
-    date.getMinutes() == config.dday.minute
-  ) {
-    const channel = await findChannel(config.dday.channel);
-    if (channel) {
-      const today = new Date();
-      const DDay = new Date(config.dday.startDate);
-      const timeGap = today.getTime() - DDay.getTime();
-      const result =
-        Math.floor(timeGap / (1000 * 60 * 60 * 24)) +
-        (config.dday.includeFirstDay ? 1 : 0);
-
-      const filter = new Collection();
-      filter.set("{dday}", String(result));
-      channel.send(replaceAll(config.dday.message, filter));
-    }
-  }
-}
-
 client.once(Events.ClientReady, (readyClient) => {
   console.log(`Ready! Logged in as ${readyClient.user.tag}`);
-  if (config.dday) setInterval(checkDDay, 60 * 1000);
 });
 
-client.on(Events.VoiceStateUpdate, async (oldState, newState) => {
-  try {
-    if (!newState.channel) return;
-    if (oldState.channelId == newState.channelId) return;
-
-    const configguild = config.guilds[newState.guild.id];
-    if (!configguild) return;
-    if (!configguild.targets.includes(newState.channelId)) return;
-
-    if (newState.channel.members.size != 1) return;
-
-    const alertChannel = await findChannel(configguild.alert);
-    if (!alertChannel) return;
-    const user = newState.member;
-    const channel = newState.channel;
-
-    const filter = new Collection();
-    filter.set("{username}", user.displayName);
-    filter.set("{channel}", channel.toString());
-    filter.set("{everyone}", "@everyone");
-
-    alertChannel.send(replaceAll(configguild.message, filter));
-  } catch (e) {
-    console.error(e);
-  }
-});
+dday(client);
+voicealert(client);
 
 client.login(config.token);
