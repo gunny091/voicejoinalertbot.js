87260c71c5137387185ab6266f6026bae0cd2796
gunny091 <74476758+gunny091@users.noreply.github.com>
2024-05-20 01:55:14 +0900

    add wait to killall command

--- commands/killall.js
+++ commands/killall.js
@@ -1,17 +1,39 @@
 import { SlashCommandBuilder } from "discord.js";
+import { sleep } from "../modules/sleep.js";
 
 export default {
   data: new SlashCommandBuilder()
     .setName("killall")
-    .setDescription("보이스에 있는사람 다 뒤지는거야"),
+    .setDescription("보이스에 있는사람 다 뒤지는거야")
+    .addIntegerOption((option) =>
+      option
+        .setName("wait")
+        .setDescription("분으로 입력하세요")
+        .setMinValue(0)
+        .setMaxValue(300)
+    ),
   async execute(interaction) {
-    if (interaction.member.voice.channel) {
-      for (const member of interaction.member.voice.channel.members.values()) {
-        await member.voice.disconnect();
+    const minutes = interaction.options.getInteger("wait", false);
+    if (minutes == 0 || minutes == null) {
+      if (interaction.member.voice.channel) {
+        for (const member of interaction.member.voice.channel.members.values()) {
+          await member.voice.disconnect();
+        }
+        await interaction.reply("처리 완료");
+      } else {
+        await interaction.reply("통방에 있어야 ㄱㄴ");
       }
-      await interaction.reply("처리 완료");
     } else {
-      await interaction.reply("통방에 있어야 ㄱㄴ");
+      interaction.reply(`${minutes}분만 ㄱㄷ`);
+      await sleep(minutes * 60 * 1000);
+      if (interaction.member.voice.channel) {
+        for (const member of interaction.member.voice.channel.members.values()) {
+          await member.voice.disconnect();
+        }
+        await interaction.channel.send("처리 완료");
+      } else {
+        await interaction.channel.send("통방에 있어야 ㄱㄴ");
+      }
     }
   },
 };
