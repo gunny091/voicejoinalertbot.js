173e50d2ace1bdf4bc3516b670e7f4020245b1fe
gunny091 <74476758+gunny091@users.noreply.github.com>
2024-05-22 00:25:52 +0900

    dday 여러개 가능

--- features/dday.js
+++ features/dday.js
@@ -8,25 +8,32 @@ const config = new JSONManager("./config.json").get();
 
 // 1분마다 디데이 체크
 async function checkDDay(client) {
-  // 시간, 분 맞는지 확인
-  const date = new Date();
-  if (date.getHours() == config.dday.hour && date.getMinutes() == config.dday.minute) {
-    // 디데이 보낼 채널 찾기
-    const channel = await findChannel(client, config.dday.channel);
-    if (channel) {
-      // 디데이 계산
-      const today = new Date();
-      const DDay = new Date(config.dday.startDate);
-      const timeGap = today.getTime() - DDay.getTime();
-      const result = Math.floor(timeGap / (1000 * 60 * 60 * 24)) + (config.dday.includeFirstDay ? 1 : 0);
+  // 설정이 한개인지 리스트인지 확인
+  let ddays = [];
+  if (!(config.dday instanceof Array)) ddays = [config.dday];
+  else ddays = config.dday;
 
-      // 필터링
-      const filter = new Collection();
-      filter.set("{dday}", String(result));
-      filter.set("{everyone}", "@everyone");
-      // 메시지 보내고 반응
-      if (config.dday.react) (await channel.send(replaceAll(config.dday.message, filter))).react(config.dday.react);
-      else await channel.send(replaceAll(config.dday.message, filter));
+  for (const dday of ddays) {
+    // 시간, 분 맞는지 확인
+    const date = new Date();
+    if (date.getHours() == dday.hour && date.getMinutes() == dday.minute) {
+      // 디데이 보낼 채널 찾기
+      const channel = await findChannel(client, dday.channel);
+      if (channel) {
+        // 디데이 계산
+        const today = new Date();
+        const DDay = new Date(dday.startDate);
+        const timeGap = today.getTime() - DDay.getTime();
+        const result = Math.floor(timeGap / (1000 * 60 * 60 * 24)) + (dday.includeFirstDay ? 1 : 0);
+
+        // 필터링
+        const filter = new Collection();
+        filter.set("{dday}", String(result));
+        filter.set("{everyone}", "@everyone");
+        // 메시지 보내고 반응
+        if (dday.react) (await channel.send(replaceAll(dday.message, filter))).react(dday.react);
+        else await channel.send(replaceAll(dday.message, filter));
+      }
     }
   }
 }
